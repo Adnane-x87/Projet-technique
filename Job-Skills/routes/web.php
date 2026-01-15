03c4e8abd2a4d5e0a94368e72ac8fcd3cd8edc28
Adnane Kes <tangerinoh40@gmail.com>
2026-01-15 15:04:08 +0000

    done

--- Job-Skills/routes/web.php
+++ Job-Skills/routes/web.php
@@ -5,17 +5,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmploiController;
 use App\Http\Controllers\SkillsController;
 
-// Redirect home to jobs list
-Route::get('/', function () {
-    return redirect()->route('emplois.index');
-});
 
-// Admin Dashboard (now public)
-Route::get('/admin', [EmploiController::class, 'manage'])->name('admin.dashboard');
-
-// All CRUD Routes (now public)
-Route::resource('emplois', EmploiController::class);
-Route::resource('skills', SkillsController::class)->only(['index', 'store', 'destroy']);
-
-// AJAX API Route
-Route::get('/api/emplois', [EmploiController::class, 'search'])->name('api.emplois.search');
